df34807a0f1d162dfc3ee08b88c4b94a354b59cc
Hari Krishna <94832444+hari890@users.noreply.github.com>
2022-03-14 14:48:59 +0530

    Create out11.py

---
+++ out11.py
@@ -0,0 +1,28 @@
+Enter matrix 1 elements
+2 3 4 5
+Printing using assignment operator
+2 3
+4 5
+Printing using copy constructor
+2 3
+4 5
+Enter matrix 2 elements
+2 3 4
+3 5 6
+First matrix is larger
+Second matrix is smaller
+Both matrices are equal
+First matrix is larger
+Both matrices are not equal
+Adding using shorthand operator
+4 6
+8 8
+Adding using binary operator
+6 9
+12 11
+Pre incrementing operators
+7 10
+13 12
+Post incrementing operators
+8 11
+14 13
