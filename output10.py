246f9ec641b69eaf02ad52f7c63bcf9ef71c78d5
Hari Krishna <94832444+hari890@users.noreply.github.com>
2022-03-14 14:46:22 +0530

    Create output10.py

---
+++ output10.py
@@ -0,0 +1,23 @@
+2nd object is larger
+1st object is smaller
+Post Incrementing
+Real value is 2
+Imaginary value is 3
+Pre Incrementing
+Real value is 4
+Imaginary value is 5
+Adding two classes
+Real value is 6
+Imaginary value is 8
+Adding using short hand operators
+Real value is 6
+Imaginary value is 8
+Assignment operator overloading
+Real value is 2
+Imaginary value is 3
+Paranthesis operator overloading
+Real value is 5
+Imaginary value is 4
+Magnitude part is 6
+Press any key to continue . . .
+
