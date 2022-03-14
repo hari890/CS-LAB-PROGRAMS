a6ac886ff0cc94851b9a770d2c662b09bb8fe07d
Hari Krishna <94832444+hari890@users.noreply.github.com>
2022-03-14 14:51:32 +0530

    Create output12.py

---
+++ output12.py
@@ -0,0 +1,31 @@
+Enter elements
+2 3 4
+3 4 5
+2 3
+4 3
+Displaying using copy constructor
+2 3
+4 3
+Displaying using assignment operator
+2 3
+4 3
+Enter elements
+4 5 6
+First matrix is larger
+First matrix is smaller
+First matrix is smaller
+Second matrix is larger
+Both matrices are not equal
+Adding using shorthand operator
+6 8
+8 8
+Adding using binary operator
+10 13
+12 13
+Pre incrementing operators
+3 4
+5 4
+Post incrementing operators
+4 5
+6 5
+Press any key to continue . . .
